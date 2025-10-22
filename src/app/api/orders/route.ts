5184660f1f39ebfd0858471f2772a5a3cfb162c9
oyunmyagmar <25LP4018@nest.edu.mn>
2025-10-23 03:37:30 +0900

    orders

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  console.log(data);
+
+  return NextResponse.json({ message: "success" });
+}
