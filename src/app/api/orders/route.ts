01aec53bb16cce8d7004b1e37b1998c09586cb4c
oyunmyagmar <25LP4018@nest.edu.mn>
2025-11-03 19:11:37 +0800

    aded

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,6 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
 import { createOrder, getAllOrders } from "@/lib/services/order-service";
 import { updateUser } from "@/lib/services/user-service";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   let orders = await getAllOrders();
@@ -24,23 +24,18 @@ export async function POST(request: NextRequest) {
     console.log("==cartFoods==", cartFoods, "==cartFoods==");
     console.log("==address==", address, "==address==");
 
-    await createOrder({ userId, cartFoods, cartItemsTotalPrice });
+    if (!userId || !cartItemsTotalPrice || !cartFoods?.length || !address) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
 
+    await createOrder({ userId, cartFoods, cartItemsTotalPrice });
     await updateUser(userId, address);
 
-    return NextResponse.json({
-      success: true,
-      message: "Data received successfully",
+    return new NextResponse("Your order has been successfully placed!", {
+      status: 201,
     });
   } catch (error) {
-    console.error("Error processing data", error);
-    return NextResponse.json(
-      {
-        success: false,
-        error: "Internel Server Error",
-        details: error instanceof Error ? error.message : "Unknown error",
-      },
-      { status: 500 }
-    );
+    console.error("Error creating order:", error);
+    return new NextResponse("Internel Server Error", { status: 500 });
   }
 }
